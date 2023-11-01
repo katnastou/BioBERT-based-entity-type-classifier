53304c0d33e96155763fd9a3758b4759c6bbaee6
Katerina Nastou <katnastou@biol.uoa.gr>
2023-11-01 15:48:29 +0100

    Update run-entity-classification.sh

--- run-entity-classification.sh
+++ run-entity-classification.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BERT_DIR="models/biobert_v1.1_pubmed"
-DATASET_DIR="5-class-125K-w100-filtered-shuffled"
+DATASET_DIR="data/biobert/example-data"
 MAX_SEQ_LENGTH="256"
 BATCH_SIZE="32"
 LEARNING_RATE="2e-5"
@@ -42,3 +42,18 @@ python3 run_ner_consensus.py \
 
 
 echo "Ready"
+
+
+# where `replace_span` is the span to replace the named entity in focus, in case of masking (no replacement is done if left undefined), 
+# `task_name` is the name of the NER task (consensus in this case), 
+# `init_checkpoint` is the checkpoint of the model (should change to the trained model during prediction), 
+# `vocab_file` is the vocabulary file of BERT (same directory as the original model), 
+# `bert_config_file` is the configuration json of the model, 
+# `data_dir` is the directory with input data, 
+# `output_dir` is the directory with output data, 
+# `eval_batch_size` and `predict_batch_size` is the batch size, 
+# `max_seq_length` is the maximum sequence length in tokens, 
+# `learning_rate`, `num_training_epochs` are the initial learning rate and the maximum number of training epochs, 
+# `cased` is the flag to define whether to run with lower case or not, based on the model, 
+# `labels_dir` is the directory with the labels file containing the labels for the supervised training task 
+# the last options (`use_xla`, `use_fp16` and `horovod`) are required for training on multi-node/multi-gpu settings
